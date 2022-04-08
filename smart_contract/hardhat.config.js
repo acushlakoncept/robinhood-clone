a38a193503b3865540419e5a739b1dd3aac05b9d
Uduak Essien <acushla4real@gmail.com>
2022-04-08 23:49:28 +0100

    Add network config to hardhat

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config({path: '.env'})
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [process.env.WALLET_PRIVATE_KEY]
+    }
+  }
+};
