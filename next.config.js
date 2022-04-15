6af498f25fcbf4820b24f497094e05ee7575f612
Uduak Essien <acushla4real@gmail.com>
2022-04-15 12:33:59 +0100

    Add env exports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    SANITY_TOKEN: process.env.SANITY_TOKEN,
+    COIN_RANKING_HOST: process.env.COIN_RANKING_HOST,
+    COIN_RANKING_API: process.env.COIN_RANKING_API,
+  }
 }
 
 module.exports = nextConfig
